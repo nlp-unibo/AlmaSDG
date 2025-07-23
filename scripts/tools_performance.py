2cc98ab639ead97f022adbb667e70f0e662ee4a3
lucabolognini <luca.bolognini6@unibo.it>
2025-07-23 19:07:39 +0200

    fixed signatures

--- scripts/tools_performance.py
+++ scripts/tools_performance.py
@@ -14,13 +14,15 @@ LLM_NAMES = ["gemini", "gpt", "llama"]
 class Eval:
 
     def __init__(self):
-        self.alma_sdg= pd.read_excel(os.path.join(os.pardir, 'datasets', 'alma-sdg.csv'))
+        self.alma_sdg= pd.read_csv(os.path.join(os.pardir, 'datasets', 'alma_sdg.csv')).sort_values(by='handle').reset_index(drop=True)
     
     def get_sdg_columns(self, df:pd.DataFrame)-> pd.DataFrame:
          return df[list(filter(lambda x: 'sdg' in x, df.columns))]
     
-    def evaluate(self, ground_truth: Union[pd.DataFrame, np.array], predictions: Union[pd.DataFrame, np.ndarray], f1_averages: Union[list[str], str], model_name: str, return_report:bool= False):
-    
+    def evaluate(self, predictions: Union[pd.DataFrame, np.ndarray], f1_averages: Union[list[str], str], model_name: str):
+        
+        ground_truth = self.alma_sdg
+
         if type(f1_averages) == str:
             f1_averages = list(f1_averages)
         
@@ -30,11 +32,9 @@ class Eval:
 
         if type(predictions) == np.ndarray:
             predictions = predictions
-            # results = classification_report(ground_truth.values, predictions, target_names=labels_names, zero_division=0.0)
         else:
-            predictions = predictions.sort_values(by='handle').reset_index(drop=True)
+            # predictions = predictions.sort_values(by='handle').reset_index(drop=True)
             predictions = predictions[labels_names].values
-            # results = classification_report(ground_truth.values, predictions[labels_names], target_names=labels_names, zero_division=0.0)
 
         results = classification_report(y_true, predictions, target_names=labels_names, zero_division=0.0)
 
@@ -42,69 +42,158 @@ class Eval:
         for average in f1_averages:
             print(f"{average}: {rounder(f1_score(y_true, predictions, average=average))}")
 
-        if return_report:
-            report = classification_report(y_true, predictions, target_names=labels_names, zero_division=0.0, output_dict=True)
-            return report
-        else:
-            return None
-
 class OSDG_eval(Eval):
 
     def __init__(self):
-        super.__init__()
-        self.predictions = pd.read_excel(os.path.join(os.pardir, 'datasets','other_tools', 'osdg_prediction.csv'))
+        super().__init__()
+        self.predictions = pd.read_csv(os.path.join(os.pardir, 'datasets','other_tools', 'osdg_prediction.csv')).sort_values(by='handle').reset_index(drop=True)
+        self.mlb = MultiLabelBinarizer(classes= np.array(range(1,18)))
+        self.model_name= 'OSDG'
+        
+        self.mlb.fit(np.ones(17, dtype='int').reshape(-1,17))
 
+    def prepare_tool_labels(self):
+        
+        labels = self.predictions['OSDG Tool']
+        prepare_labels = lambda x: list(map(float, str(x).replace('nan', '0').split(',')))
+        labels = labels.apply(prepare_labels)
 
-    def prepare_tool_labels():
-         
-        prepare_labels = lambda x: tuple(map(float, str(x).replace('nan', '0').split(',')))
+        y_hat = self.mlb.transform(labels)
 
+        return y_hat
 
-class Elsevier_eval(Eval):
+    def evaluate(self, f1_averages:list[str] = ['micro', 'macro', 'weighted']):
+        assert (self.alma_sdg.handle == self.predictions.handle).all()
+
+        predictions = self.prepare_tool_labels()
+        return super().evaluate(predictions, f1_averages, self.model_name)
+    
+class ElsevierMulti_eval(Eval):
 
     def __init__(self):
         super().__init__()
-        self.predictions = pd.read_excel(os.path.join(os.pardir, 'datasets','other_tools', 'elsevier-sdg-multi.csv'))
-    
-    def get_singlelabel_prediction(self):
+        self.predictions = pd.read_csv(os.path.join(os.pardir, 'datasets','other_tools', 'elsevier-sdg-multi.csv')).sort_values(by='handle').reset_index(drop=True)
+        self.model_name = 'Elsevier-Multilabel'
 
     def get_multilabel_prediction(self, threshold:float= 0.1):
         
-        binarized_predictions = self.transform_probability(self.predictions, threshold)
+        hat_sdg = super().get_sdg_columns(self.predictions)
+        y_hat = self.transform_probability(hat_sdg, threshold)
+
+        return y_hat.to_numpy()
+
+    def transform_probability(self, df:pd.DataFrame, threshold:float) -> pd.DataFrame:
+        return df.map(lambda x: True if x>= threshold else False)
+   
+    def evaluate(self, f1_averages:list[str] = ['micro', 'macro', 'weighted']):
+        assert (self.alma_sdg.handle == self.predictions.handle).all()
+
+        predictions = self.get_multilabel_prediction()
+        return super().evaluate(predictions, f1_averages, self.model_name)
+
+class ElsevierSingle_eval(Eval):
+
+    def __init__(self):
+        super().__init__()
+        self.predictions = pd.read_csv(os.path.join(os.pardir, 'datasets','other_tools', 'elsevier-sdg-multi.csv')).sort_values(by='handle').reset_index(drop=True)
+        self.model_name= 'ElsevierSingle'
+    
+    def get_singlelabel_prediction(self):
+        hat_sdg = self.get_sdg_columns(self.predictions)        
+        pred = np.zeros_like(hat_sdg)
+        pred[np.arange(hat_sdg.shape[0]), np.nanargmax(hat_sdg, axis = 1)] = 1 
+
+        return pred
 
     def transform_probability(self, df:pd.DataFrame, threshold:float) -> pd.DataFrame:
         return df.map(lambda x: True if x>= threshold else False)
 
-    
+    def evaluate(self, f1_averages:list[str] = ['micro', 'macro', 'weighted']):
+        assert (self.alma_sdg.handle == self.predictions.handle).all()
 
+        predictions = self.get_singlelabel_prediction()
+        return super().evaluate(predictions, f1_averages, self.model_name)
+    
 class Aurora_eval(Eval):
 
     def __init__(self):
         super().__init__()
+        self.model_name = 'Aurora'
+        self.predictions = pd.read_csv(os.path.join(os.pardir, 'datasets','other_tools', 'aurora-sdg-multi.csv')).sort_values(by='handle').reset_index(drop=True)
     
+    def transform_probability(self, df:pd.DataFrame, threshold:float) -> pd.DataFrame:
+        return df.map(lambda x: True if x>= threshold else False)
+    
+    def binarize_predictions(self):
+        hat_sdg = super().get_sdg_columns(self.predictions)
+        hat_sdg = self.transform_probability(hat_sdg, 0.5)
 
-class LLM_eval(Eval):
+        return hat_sdg.to_numpy()
+
+    def evaluate(self, f1_averages:list[str] = ['micro', 'macro', 'weighted']):
+        assert (self.alma_sdg.handle == self.predictions.handle).all()
+        
+        predictions = self.binarize_predictions()
+        return super().evaluate(predictions, f1_averages, self.model_name)
+
+class GPT_eval(Eval):
 
     def __init__(self):
         super().__init__()
-        
+        self.model_name = 'gpt'
+        self.predictions = pd.read_excel(os.path.join(os.pardir, 'datasets','other_tools', 'gpt_answers_20082024_t00.xlsx'), sheet_name='ProcessedOutput').sort_values(by='handle').reset_index(drop=True)
+    
+    def evaluate(self):
+        assert (self.alma_sdg.handle == self.predictions.handle).all()
+
+        predictions = self.predictions
+        f1_averages = ['micro', 'macro', 'weighted']
+        model_name = self.model_name
+        return super().evaluate(predictions, f1_averages, model_name)
+    
+class Gemini_eval(Eval):
+
+    def __init__(self):
+        super().__init__()
+        self.model_name = 'gemini'
+        self.predictions = pd.read_excel(os.path.join(os.pardir, 'datasets','other_tools', 'gemini_results_temp00_270824.xlsx'), sheet_name='ProcessedOutput').sort_values(by='handle').reset_index(drop=True)
+        # self.alma_sdg = self.alma_sdg.sort_values(by='handle').reset_index(drop=True)
+    
+    def evaluate(self):
+        assert (self.alma_sdg.handle == self.predictions.handle).all()
+
+        predictions = self.predictions
+        f1_averages = ['micro', 'macro', 'weighted']
+        model_name = self.model_name
+        return super().evaluate(predictions, f1_averages, model_name)
+    
+class LLama_eval(Eval):
+
+    def __init__(self):
+        super().__init__()
+        self.model_name = 'llama'
+        self.predictions = pd.read_excel(os.path.join(os.pardir, 'datasets','other_tools', 'llama_01_05_25.xlsx'), sheet_name='ProcessedOutput').sort_values(by='handle').reset_index(drop=True)
+        # self.alma_sdg = self.alma_sdg.sort_values(by='handle').reset_index(drop=True)
 
 
-osdg_labels = pd.Series((map(prepare_tool_labels, osdg_outputs['OSDG Tool'])), index = osdg_outputs['handle'],)
+    def evaluate(self):
+        assert (self.alma_sdg.handle == self.predictions.handle).all()
 
-mlb = MultiLabelBinarizer(classes= np.array(range(1,18)))
-mlb.fit(np.ones(17, dtype='int').reshape(-1,17))
+        predictions = self.predictions
+        f1_averages = ['micro', 'macro', 'weighted']
+        model_name = self.model_name
+        return super().evaluate(predictions, f1_averages, model_name)
 
-osdg_results = pd.DataFrame(mlb.transform(osdg_labels), columns = labels_names)
-osdg_results['handle'] = osdg_labels.index
+if __name__ == 'main':
 
-llm_prf_path = os.path.join(os.curdir, 'notebooks', 'performance_json', 'Other_tools', 'llms')
+    other_tools = {'osdg': OSDG_eval(), 
+                'elsevierSingleLabel' : ElsevierSingle_eval(),
+                'elsevierMultiLabel' : ElsevierMulti_eval(),
+                'aurora' : Aurora_eval(),
+                'gpt':GPT_eval(),
+                'gemini' :  Gemini_eval(),
+                'llama' :  LLama_eval()}
 
-for llm in LLM_NAMES:
-    rep = evaluate(gold_dataset, annotation_sheets[llm], ['micro', 'macro', 'weighted'], llm, return_report=True)
-    save_path = os.path.join(llm_prf_path, llm + '.json')
-    with open(save_path, 'w') as f:
-        json.dump(rep, f, indent=3)
-
-
-evaluate(gold_dataset, osdg_results, ['micro', 'macro', 'weighted'], 'osdg')# Check if alignment is correct
+    for model_name, model in other_tools.items():
+        model.evaluate()
+        print('\n')
